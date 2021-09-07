2fe7a537a53a7721e5644747397157fe785d24ad
Alex <esarev@inbox.ru>
2021-09-07 21:47:04 +0700

    upd_tabs

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,8 +1,9 @@
 const modals = () => {
-  function blindModal(triggerSelector, modalSelector, closeSelector) {
+  function blindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
     const trigger = document.querySelectorAll(triggerSelector),
           modal = document.querySelector(modalSelector),
-          close = document.querySelector(closeSelector);
+          close = document.querySelector(closeSelector),
+          windows = document.querySelectorAll('[data-modal]');
 
     trigger.forEach(item => {
       item.addEventListener('click', (e) => {
@@ -10,18 +11,30 @@ const modals = () => {
           e.preventDefault();
         }
 
+        windows.forEach(item => {
+          item.style.display = 'none';
+        });
+
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
       });
     });
 
     close.addEventListener('click', () => {
+      windows.forEach(item => {
+        item.style.display = 'none';
+      });
+      
       modal.style.display = 'none';
       document.body.style.overflow = '';
     });
 
     modal.addEventListener('click', (e) => {
-      if(e.target === modal) {
+      if(e.target === modal && closeClickOverlay) {
+        windows.forEach(item => {
+          item.style.display = 'none';
+        });
+
         modal.style.display = 'none';
         document.body.style.overflow = '';
       }
@@ -37,6 +50,9 @@ const modals = () => {
 
   blindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   blindModal('.phone_link', '.popup', '.popup .popup_close');
+  blindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
+  blindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
+  blindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
   // showModalTime('.popup', 60000);
 };
 
