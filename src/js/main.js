210a7f004b2c2eb5d56a2b71082d6e36b6ebda1a
Alex <esarev@inbox.ru>
2021-09-15 19:23:56 +0700

    imgPopup

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,13 @@ import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
 import timer from './modules/timer';
+import images from './modules/images';
 
 window.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
   let modalState = {};
-  let deadline = '2021-11-01 00:00';
+  let deadline = '2021-12-18 00:00';
 
   changeModalState(modalState);
   modals();
@@ -18,4 +19,5 @@ window.addEventListener('DOMContentLoaded', () => {
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   forms(modalState);
   timer('.container1', deadline);
+  images();
 });
